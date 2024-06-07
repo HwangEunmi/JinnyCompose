bb3992e7b8504f8649a0a8aea296c93232b1b714
지니황 <eunmi@jinihwang-ui-MacBookPro.local>
2024-06-07 23:00:01 +0900

    Custom AppBar 컴포넌트 추가

---
+++ app/src/main/java/com/study/jinnycompose/ui/component/AppBar.kt
@@ -0,0 +1,55 @@
+package com.study.jinnycompose.ui.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@ExperimentalMaterial3Api
+@Composable
+fun AppBar(
+    title: String?,
+    appBarColor: @Composable () -> TopAppBarColors,
+    onNavigationIconClick: () -> Unit,
+    onLikeIconClick: () -> Unit,
+    onShareIconClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = {
+            title?.let { Text(title) }
+        },
+        navigationIcon = {
+            IconButton(onClick = onNavigationIconClick) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowLeft,
+                    contentDescription = "keyboardArrowLeft Button"
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = onLikeIconClick) {
+                Icon(
+                    imageVector = Icons.Default.FavoriteBorder,
+                    contentDescription = "like Button"
+                )
+            }
+            IconButton(onClick = onShareIconClick) {
+                Icon(
+                    imageVector = Icons.Default.Share,
+                    contentDescription = "share button"
+                )
+            }
+        },
+        colors = appBarColor(),
+        modifier = modifier
+    )
+}
