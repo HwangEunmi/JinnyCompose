72787a103bed26fe5264fbef6b484a6f05f97a15
지니황 <eunmi@jinihwang-ui-MacBookPro.local>
2024-06-10 23:56:42 +0900

    스크롤 상태를 가져오는 함수

--- app/src/main/java/com/study/jinnycompose/utils/ScrollUtils.kt
+++ app/src/main/java/com/study/jinnycompose/utils/ScrollUtils.kt
@@ -1,3 +1,27 @@
+package com.study.jinnycompose.utils
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import com.study.jinnycompose.ui.data.ScrollDirection
+import com.study.jinnycompose.ui.data.ScrollState
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+
+// TODO(jinny): by remember vs by remember(key)의 차이점
+// TODO(jinny): derivedStateOf vs snapshotFlow
+// TODO(jinny): offset에 대해 정확히 알아보기 (firstVisibleItemOffset이 계속 쓰이는 이유)
+// TODO(jinny): LaunchedEffect vs rememberCoroutineScope의 사용 차이점
 @ExperimentalCoroutinesApi
 @Composable
 fun LazyListState.getScrollDirection(): ScrollDirection {
@@ -25,4 +49,31 @@ fun LazyListState.getScrollDirection(): ScrollDirection {
             }
     }
     return scrollDirection
+}
+
+@ExperimentalCoroutinesApi
+@Composable
+fun LazyListState.getScrollStateAfterStop(): ScrollState {
+    var scrollState by remember {
+        mutableStateOf(ScrollState.Stop)
+    }
+
+    LaunchedEffect(this) {
+        snapshotFlow { isScrollInProgress }
+            .distinctUntilChanged()
+            .flatMapLatest { isScrolling ->
+                when (isScrolling) {
+                    true -> {
+                        flowOf(ScrollState.Scrolling)
+                    }
+                    false -> {
+                        delay(500)
+                        flowOf(ScrollState.Stop)
+                    }
+                }
+            }.collectLatest { state ->
+                scrollState = state
+            }
+    }
+    return scrollState
 }
