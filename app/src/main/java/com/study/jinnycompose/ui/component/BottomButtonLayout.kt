f375490d48af9404e44cafa3655910adb288806c
지니황 <eunmi@jinihwang-ui-MacBookPro.local>
2024-06-10 23:58:00 +0900

    하단 버튼 레이아웃 추가

---
+++ app/src/main/java/com/study/jinnycompose/ui/component/BottomButtonLayout.kt
@@ -0,0 +1,54 @@
+package com.study.jinnycompose.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BottomButtonLayout(
+    layoutHeight: Dp,
+    onButtonClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(layoutHeight)
+            .background(Color(0xFFFAEBD7))
+    ) {
+        Text(
+            text = "33,000원",
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 20.dp)
+        )
+
+        Button(
+            modifier = Modifier
+                .align(Alignment.CenterEnd),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFFF8E7F)
+            ),
+            onClick = { onButtonClick() }
+        ) {
+            Text(
+                text = "신청하기"
+            )
+        }
+    }
+}
